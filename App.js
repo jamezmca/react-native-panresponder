98d28cf9fbafb1cb3b5d0e1d38d7af3d23a25db2
James McArthur <jamesmca@live.com>
2021-04-06 15:08:59 -0700

    Update App.js
    colored & numbered list with random colors and each with an '@' handler

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const colorMap = {}
 
 export default function App() {
+  const [array, setArray] = useState({
+    data: Array.from(Array(200), (_, i) => {
+      colorMap[i] = getRandomColor()
+      return i
+    })
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList data={array.data}
+        style={{ width: '100%' }}
+        renderItem={({ item }) => (
+          <View style={{ padding: 16, backgroundColor: colorMap[item], flexDirection: 'row' }}>
+            <View>
+              <Text style={{fontSize: 28}}>@</Text>
+            </View>
+            <Text style={{fontSize: 22, textAlign: 'center', flex: 1}}>{item}</Text>
+          </View>)}
+        keyExtractor={(item) => "" + item} />
       <StatusBar style="auto" />
     </View>
   );
